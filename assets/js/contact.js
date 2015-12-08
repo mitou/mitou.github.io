fe6f624fa435e195eed2acfaac08c8c0be88e26b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2015-12-08 13:39:49 +0900

    send contact info to the old server

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -23,7 +23,7 @@
 
 				$.ajax({
 					type: 'POST',
-					url: 'assets/php/contact.php',
+					url: 'http://ja-jp-website-mitou.azurewebsites.net/assets/php/contact.php',
 					dataType: 'json',
 					data: {
 						name: name,
